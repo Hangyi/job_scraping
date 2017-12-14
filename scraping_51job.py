b6bd54e81af4cffeb34ffec8e8a50ac169705832
Hangyi <sweat0909@gmail.com>
2017-12-14 23:49:38 +0800

    输出到excel

--- scraping_51job.py
+++ scraping_51job.py
@@ -1,23 +1,26 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import xlwt
 
-def get_content(url):
+def get_content(page):
     """获取网页源码"""
+    url = "http://search.51job.com/list/080200,000000,0000,00,9,99,%25E8%25BD%25AF%25E4%25BB%25B6%25E6%25B5%258B%25E8%25AF%2595,2," + page + ".html?"
     a = urlopen(url)
     html = a.read().decode('gbk')
     return html
 
 def get_bsObj(html):
     """获取bsObj对象"""
-    bsObj = BeautifulSoup(html. "lxml")
+    bsObj = BeautifulSoup(html, "lxml")
     return bsObj
 
 def get_position(html):
     reg = re.compile(r'class="t1 ">.*? <a target="_blank" title="(.*?)".*? <span class="t2"><a target="_blank" title="(.*?)".*?<span class="t3">(.*?)</span>.*?<span class="t4">(.*?)</span>.*? <span class="t5">(.*?)</span>',re.S)
     items = re.findall(reg, html)
-    for i in items:
-        print(i[0]+'\t'+i[1]+'\t'+i[2]+'\t'+i[3]+'\t'+i[4]+'\n')
+    # for i in items:
+    #     print(i[0]+'\t'+i[1]+'\t'+i[2]+'\t'+i[3]+'\t'+i[4]+'\n')
+    return items
 
 # 这里操作的是bsObj对象
 def get_num_position(bsObj):
@@ -37,9 +40,32 @@ def get_num_page(html):
     num_page = items[0][0]
     return(num_page)
 
-url = "http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=080200&keyword=%E8%BD%AF%E4%BB%B6%E6%B5%8B%E8%AF%95"
-html = get_content(url)
-num_page = get_num_page(html)
-for page in int(num_page):
+# 将结果输出到excel
+def excel_write(items,index):
+    # 爬取到的内容写入excel表格
+    for item in items: # 职位信息
+        for i in range(0,5):
+            # print item[i]
+            ws.write(index,i,item[i], xlwt.easyxf('font: name 微软雅黑')) # 行，列，数据
+        # print(index)
+        index+=1
+
+
+# url = "http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=080200&keyword=%E8%BD%AF%E4%BB%B6%E6%B5%8B%E8%AF%95"
+html = get_content("1")
+num_page = int(get_num_page(html))
+for page in range(1, num_page):
     print("正在爬取" + str(page) + "页数据...")
-    get_
+    content = get_content(str(page))
+    get_position(content)
+
+wb = xlwt.Workbook()
+ws = wb.add_sheet('A Test Sheet')
+headData = ['招聘职位', '公司', '地址', '薪资', '日期'] # 表头信息
+for column in range(0, 5):
+    ws.write(0, column, headData[column], xlwt.easyxf('font: name 微软雅黑, bold on')) # 行，列
+
+for i in range(1, num_page):
+    index = (i - 1) * 50 + 1
+    excel_write(get_position(get_content(str(i))), index)
+wb.save('result1.xls')
