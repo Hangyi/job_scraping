cc8763aa337d0a04b7e0aa3f368301ea2893c913
Hangyi <sweat0909@gmail.com>
2018-03-27 03:58:33 +0800

    add shell script

---
+++ 001. job_scraping/51job_searcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program:
+#     This program run scraping_51job.py
+# History:
+# 2018/03/22 Hangyi  First release
+# 2018/03/24 Hangyi  add path
+# 2018/03/27 Hangyi  delete path
+
+cd /home/hangyi/scripts/job_searcher
+python3 scraping_51job.py
+exit 0
